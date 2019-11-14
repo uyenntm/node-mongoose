13773bad1200e4c956f8e801065455f10370310a
Natalie Nguyen <uyenntm2001@gmail.com>
2019-11-13 20:06:13 -0800

    Mongoose Part 3

--- index.js
+++ index.js
@@ -7,21 +7,39 @@ connect.then((db)=>{
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: "Uthappizza",
+        name: 'Uthappizzaza',
         description: 'test'
     })
-    .then((dish)=>{
+    .then((dish) => {
         console.log(dish);
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return Dishes.remove({});
     })
-    .then(()=>{
+    .then(() => {
         return mongoose.connection.close();
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err);
-    })
+    });
 });
